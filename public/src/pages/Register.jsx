05d3ee999752e677f111fecdb23fd2002a2f0f49
AKSHAT SHARMA <akshatgopal70@gmail.com>
2024-10-13 00:37:02 +0530

    register pg frontend done

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,11 +1,186 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 function Register() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const toastPreferences = {
+    position: "bottom-right",
+    autoClose: 3000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error("Both passwords should match", toastPreferences);
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should contain at least 2 characters",
+        toastPreferences
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should contain at least 8 characters",
+        toastPreferences
+      );
+      return false;
+    } else if (email === "") {toast.error("Email is required", toastPreferences);return false;}
+    return true;
+  };
+
+  const handleSubmit  = async (event) => {
+    event.preventDefault();
+    if(handleValidation()){
+        const { password, confirmPassword, username, email } = values;
+        const data = await axios.post();
+    }
+  };
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleSubmit(event);
+    }
+  };
+
   return (
-    <div>
-      Register
-    </div>
-  )
+    <>
+      <FormContainer>
+        <ToastContainer />
+        <form
+          onKeyDown={handleKeyDown}
+          onSubmit={(event) => handleSubmit(event)}
+        >
+          <div className="brand">
+            <img src={Logo} alt="" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an Account? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
 }
 
-export default Register
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: pink;
+      text-decoration: none;
+      font-weight: bold;
+    }
+
+    a:hover {
+      transition: 0.5s ease-in-out;
+      color: magenta;
+    }
+  }
+`;
+
+export default Register;
